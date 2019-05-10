0b86d96156912eb42abbe323288b652a0c338c86
Cali Tabilas <ctabilas23@gmail.com>
2019-05-09 21:12:19 -0400

    setting up the server data

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const MongoClient = require("mongodb").MongoClient;
+const bodyParser = require("body-parser");
+
+// TODO: add to the db specifically MAYBE FIX THIS
+// const db = require("config.js");
+const url = "mongodb://localhost:27017";
+// const db = client.db("mern_shopping");
+let db;
+
+const app = express();
+
+app.use(bodyParser.JSON());
+app.use(bodyParser.urlencoded());
+
+// Database connections
+MongoClient.connect(url)
+  .then(client => {
+    const db = client.db("mern-shopping");
+    // db.collection("exhibit").insertOne({
+    //   species: "Lion",
+    //   name: "Fred",
+    //   age: 12
+    // });
+  })
+  .catch(err =>
+    console.error(err, "There was an error inserting into the database")
+  );
+
+MongoClient.connect(url)
+  .then(client => {
+    db = client.db("mern_shopping");
+  })
+  .catch(err =>
+    console.error(err, "There was an error connecting to the database")
+  );
